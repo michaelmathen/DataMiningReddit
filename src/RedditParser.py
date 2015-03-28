acaac119c9dcb5f895f93971e8d02dfef29983c4
Michael Matheny <michaelmatheny2.71@gmail.com>
2015-03-28 14:00:21 -0600

    updated the code so that you can get streams from users

--- src/RedditParser.py
+++ src/RedditParser.py
@@ -15,9 +15,36 @@ comment_map = dict(zip(["body",
                        count()))
 
 
-def Parse_Subreddit(name):
+def Get_Hot(subreddit_handle):
+    return subreddit_handle.get_hot(limit=None)
+
+
+def Get_New(subreddit_handle):
+    return subreddit_handle.get_new(limit=None)
+
+
+def Get_Top(subreddit_handle):
+    return subreddit_handle.get_top(limit=None)
+
+
+def Get_Rising(subreddit_handle):
+    return subreddit_handle.get_rising(limit=None)
+
+
+def Get_Controversial(subreddit_handle):
+    return subreddit_handle.get_controversial(limit=None)
+
+
+
+
+def Parse_Subreddit(name, order=Get_Hot):
+    """
+    Given the name of a subreddit get all comments ever posted in that subreddit.
+    The optional order determines the order in which they are returned.
+    The functions above can be passed in to change the order.
+    """
     r = praw.Reddit(USER_STR)
-    for submission in r.get_subreddit(name).get_hot(limit=None):
+    for submission in order(r.get_subreddit(name)):
         for comment_obj in praw.helpers.flatten_tree(submission.comments):
             if isinstance(comment_obj, praw.objects.MoreComments):
                 #I am just ignoring these
@@ -37,3 +64,33 @@ def Parse_Subreddit(name):
             except:
                 print "Bad comment. I am not sure why this happens?"
                 continue
+
+
+def All_User_Comments(user_name, sort='new'):
+    """
+    Give an username this function will return all comments that the user 
+    has ever posted formatted in the same way that we use for Parse_Subreddit.
+    user_name -- The name of the user.
+    sort -- The order in which the comments are returned.
+    """
+    r = praw.Reddit(USER_STR)
+    for comment_obj in r.get_redditor(user_name).get_comments(sort=sort,limit=None):
+        try:
+            if isinstance(comment_obj, praw.objects.MoreComments):
+                continue
+            submission= r.get_submission(submission_id=comment_obj.link_id[3:])
+            comment_data = (comment_obj.body, 
+                            comment_obj.gilded,
+                            comment_obj.score,
+                            comment_obj.created_utc,
+                            comment_obj.subreddit.display_name,
+                            comment_obj.author.name,
+                            comment_obj.link_title,
+                            comment_obj.link_author,
+                            submission.score,
+                            submission.created_utc)
+            yield comment_data
+        except:
+            print "Bad comment."
+            continue
+        
--- src/main.py
+++ src/main.py
@@ -2,6 +2,12 @@ import RedditParser
             
 
 if __name__ == "__main__":
-    for comment in RedditParser.Parse_Subreddit("math"):
+    #r = RedditParser.praw.Reddit(RedditParser.USER_STR)
+    #print r.get_submission(submission_id='20gewk')
+    for comment in RedditParser.All_User_Comments("_vargas_"):
         print comment
+
+        
+        #for comment in RedditParser.Parse_Subreddit("math"):
+        #print comment
 
